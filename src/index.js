66b54e69069732166997f197c102fdbaf499dd24
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-09-07 12:32:12 +0600

    Created reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
