bde45da78b96856baa2924d3dede259d02eafbda
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-09-08 13:42:03 +0600

    Implemented error handling on incorrect symbols

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,7 +5,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     if (action.type === 'CONCAT_CHAR') {
-        return {...state, counter: state.counter + action.payload};
+        let newCounter = state.counter;
+        let newPayload = action.payload;
+
+        if (state.counter.length === 0 && isNaN(action.payload)) {
+            newPayload = '';
+        }
+
+        if (isNaN(action.payload) && isNaN(state.counter[state.counter.length - 1])) {
+            newCounter = state.counter.slice(0, state.counter.length - 1);
+        }
+
+        return {...state, counter: newCounter + newPayload};
     }
 
     if (action.type === 'CLEAR') {
@@ -13,8 +24,14 @@ const reducer = (state = initialState, action) => {
     }
 
     if (action.type === 'COUNT') {
-        const total = eval(state.counter);
-        return {...state, total};
+        let newCounter = state.counter;
+
+        if (isNaN(state.counter[state.counter.length - 1])) {
+            newCounter = state.counter.slice(0, state.counter.length - 1);
+        }
+
+        const total = eval(newCounter);
+        return {...state, counter: newCounter, total};
     }
 
     if (action.type === 'REMOVE_CHAR') {
